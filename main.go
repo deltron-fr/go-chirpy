bfc7bd38dbf599934f4804c5480707c96a7ea20a
deltron-fr <kayceoogbonnaya2304@gmail.com>
2025-10-17 21:54:26 +0100

    added routes for the application

--- main.go
+++ main.go
@@ -8,12 +8,23 @@ import (
 func main() {
 	const port = "8080"
 
+	serverHandler := http.FileServer(http.Dir("."))
+
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+
+		w.Write([]byte("OK"))
+	})
+
+	mux.Handle("/app/", http.StripPrefix("/app/", serverHandler))
+	
 	server := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
 	}
-
+	
 	log.Printf("Serving on port %s\n", port)
 	log.Fatal(server.ListenAndServe())
 }
