0da70b17c1e0b96e12f054f52ad9624ea44abbea
deltron-fr <kayceoogbonnaya2304@gmail.com>
2025-10-17 23:55:46 +0100

    added functionality for checking server hits

--- main.go
+++ main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"log"
+	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	}) 
+
+}
 
 func main() {
 	const port = "8080"
 
-	serverHandler := http.FileServer(http.Dir("."))
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-
-		w.Write([]byte("OK"))
-	})
-
-	mux.Handle("/app/", http.StripPrefix("/app/", serverHandler))
 	
+	mux := http.NewServeMux()
+
+	serverHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(serverHandler))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerServerHits)
+	mux.HandleFunc("/reset", apiCfg.handlerResetHits)
+
 	server := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
@@ -27,4 +40,25 @@ func main() {
 	
 	log.Printf("Serving on port %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerServerHits(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+
+		hits := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+		w.Write([]byte(hits))
+}
+
+func (cfg *apiConfig) handlerResetHits(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusOK)
+
+		cfg.fileserverHits.Swap(0)
 }
